c988bc9c25014af4187f03b72b08a030fbcae4c3
Bowen Chen <bwchen0719@gmail.com>
2019-12-20 22:21:16 -0600

    Add summary function

---
+++ R/summary.endoSwitch.R
@@ -0,0 +1,57 @@
+#' Summarize the endogenous switching regression results.
+#'
+#' @param object Estimated endogenous switching regression model.
+#'
+#' @return A list containing the key regression results.
+
+summary.endoSwitch <- function(object){
+
+  coefEst <- object$MLE.Results$estimate
+  coefSD <- sqrt(-diag(solve(object$MLE.Results$hessian)))
+  tStat <- coefEst/coefSD
+  pValue <- 2*pnorm(-abs(tStat))
+
+  results <- cbind("Estimate" = round(coefEst, 4), 'Std. error' = round(coefSD, 4),
+                   't value' = round(tStat, 4), 'Pr(> t)' = round(pValue, 4))
+  distPar <- object$distPar
+  distPar <- cbind(distPar, 2*pnorm(-abs(distPar[,3])))
+
+  results[(nrow(results) - 3):nrow(results), ] <- distPar
+  row.names(results)[(nrow(results) - 3):nrow(results)] <- c('Outcome.0.Sigma', 'Outcome.1.Sigma',
+                                                             'Outcome.0.Rho', 'Outcome.1.Rho')
+  treatEffect <- object$treatEffect
+  if(is.list(treatEffect)){treatEffect <- 'Treatment effects are not calculated. Use treatEffect = T to calculate them'}
+
+  summary <- list(maxinType = object$MLE.Results$type,
+                  iterations = object$MLE.Results$iterations,
+                  returnCode = object$MLE.Results$code,
+                  returnMessage = object$MLE.Results$message,
+                  loglik = object$MLE.Results$maximum,
+                  constrains = object$MLE.Results$constraints,
+                  estimate = results,
+                  treatmentEffect = treatEffect)
+  summary
+}
+
+# print.summary.endoSwitch <- function(x, print = T){
+#   coefEst <- object$MLE.Results$estimate
+#   coefSD <- sqrt(-diag(solve(object$MLE.Results$hessian)))
+#   tStat <- coefEst/coefSD
+#   pValue <- 2*pnorm(-abs(tStat))
+#
+#   results <- cbind("Estimate" = round(coefEst, 4), 'Std. error' = round(coefSD, 4),
+#                    't value' = round(tStat, 4), 'Pr(> t)' = round(pValue, 4))
+#
+#   distPar <- object$distPar
+#   distPar <- cbind(distPar, 2*pnorm(-abs(distPar[,3])))
+#
+#   results[(nrow(results) - 3):nrow(results), ] <- distPar
+#   row.names(results)[(nrow(results) - 3):nrow(results)] <- c('Outcome.0.Sigma', 'Outcome.1.Sigma',
+#                                                              'Outcome.0.Rho', 'Outcome.1.Rho')
+#
+#   cat('--------------------------------------------\n')
+#   cat('Maximum Likelihood Estimation\n')
+#   cat('Log-Likelihood:', x$MLE.Results$maximum, '\n')
+#   cat('Coefficient estimates:\n')
+#   results
+# }
